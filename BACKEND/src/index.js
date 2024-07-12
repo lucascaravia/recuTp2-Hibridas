ade299554d5156684f0ef481339fe41e148eacd5
lucascaravia <88347319+lucascaravia@users.noreply.github.com>
2024-07-12 12:31:06 -0300

    update code

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -6,3 +6,16 @@ app.listen(4000)
 
 console.log('Server on port', 4000)
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    // Aquí puedes agregar tu lógica de autenticación
+    if (email === 'usuario@example.com' && password === 'password123') {
+      res.status(200).json({ message: 'Login exitoso', token: 'fake-jwt-token' });
+    } else {
+      res.status(401).json({ message: 'Credenciales incorrectas' });
+    }
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+  });
--- BACKEND/src/proyecto2db.js
+++ BACKEND/src/proyecto2db.js
@@ -1,14 +1,11 @@
-import mongoose from "mongoose";
-import express from "express";
-
-
-const app = express();
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://admin:VG6rjaeouog82q4c@database.p2gtu3q.mongodb.net/todo_list_db?retryWrites=true&w=majority&appName=Database.');
+        await mongoose.connect('mongodb+srv://admin:VG6rjaeouog82q4c@database.p2gtu3q.mongodb.net/todo_list_db?retryWrites=true&w=majority&appName=Database.', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log(">>>>> DB is connected");
     } catch (error) {
-        console.log(error);
+        console.error("Database connection error:", error);
     }
 };
-
